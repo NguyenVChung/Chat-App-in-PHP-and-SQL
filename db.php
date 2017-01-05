f1ba3a5cf972fdf999e3cfec0a4b83daf9dc7dba
Nguyen Van Chung <chungnv02@gmail.com>
2017-01-05 10:24:09 +0700

    db.php
    connect html file with the database

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user ="root";
+$pass= "";
+$db_name = "chat_app";
+$con = new mysqli($host,$user,$pass,$db_name);
+
+function formatDate($date){
+	return date('g:i a', strtotime($date));
+}
+?>
